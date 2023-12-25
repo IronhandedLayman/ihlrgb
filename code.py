4cbfd91cff0bd23527350e6418d9d0409da572ad
ironhandedlayman <leadhyena@gmail.com>
2023-12-24 23:16:05 -0600

    major update, MVP

--- code.py
+++ code.py
@@ -10,13 +10,14 @@ import rtc
 import json
 import board
 import busio
-from digitalio import DigitalInOut
+from digitalio import DigitalInOut, Direction, Pull
 import framebufferio
 import displayio
 import rgbmatrix
 import adafruit_display_text.label
 import terminalio
 import time
+import random
 from adafruit_datetime import datetime
 import adafruit_requests as requests
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
@@ -37,8 +38,161 @@ EXIT_STATE = "EXIT_STATE"
 
 DISPLAY_BOOTING = "Booting"
 DISPLAY_DEMO1 = "Demo1"
+DISPLAY_GOL = "Gol"
+DISPLAY_CAG = "CAG"
+DISPLAY_WEATHER = "Weather"
 
-IHLRGB_VERSION = "v0.0.2"
+DISPLAY_LIST = [DISPLAY_DEMO1, DISPLAY_GOL, DISPLAY_CAG]
+
+IHLRGB_VERSION = "v0.1.0"
+
+
+class ChaserAnimationGroup:
+    def __init__(self, name):
+        self.tick = [[random.randint(0, 9) for _ in range(64)] for _ in range(32)]
+        self.tock = [[random.randint(0, 9) for _ in range(64)] for _ in range(32)]
+
+        self.palette = displayio.Palette(10)
+        for i in range(10):
+            self.palette[i] = 0x060402 * (i + 1)
+
+        self.bitmap = displayio.Bitmap(64, 32, len(self.palette))
+        self.tilegrid = displayio.TileGrid(
+            bitmap=self.bitmap, pixel_shader=self.palette
+        )
+        self.group = displayio.Group()
+        self.group.append(self.tilegrid)
+
+        self.directions = [
+            (63, 31),
+            (0, 31),
+            (1, 31),
+            (63, 0),
+            (1, 0),
+            (63, 1),
+            (0, 1),
+            (1, 1),
+        ]
+
+        self.ticktock = False
+
+    def update_anim(self):
+        if self.ticktock:
+            for y in range(32):
+                for x in range(64):
+                    inside = (1 + self.tick[y][x]) % 10
+                    for d in self.directions:
+                        if self.tick[(y + d[1]) % 32][(x + d[0]) % 64] == inside:
+                            self.tock[y][x] = inside
+                            break
+            for y in range(32):
+                for x in range(64):
+                    self.bitmap[x, y] = self.tock[y][x]
+            self.ticktock = False
+        else:
+            for y in range(32):
+                for x in range(64):
+                    inside = (1 + self.tock[y][x]) % 10
+                    for d in self.directions:
+                        if self.tock[(y + d[1]) % 32][(x + d[0]) % 64] == inside:
+                            self.tick[y][x] = inside
+                            break
+            for y in range(32):
+                for x in range(64):
+                    self.bitmap[x, y] = self.tick[y][x]
+            self.ticktock = True
+
+    def group(self):
+        return self.group
+
+    def name(self):
+        return self.name
+
+    def set_text(self, lineno, text):
+        pass
+
+
+class GameOfLifeAnimationGroup:
+    def __init__(self, name, colors=(0x080808, 0xC00000), initPattern=None):
+        self.tick = [[random.randint(0, 1) for _ in range(64)] for _ in range(32)]
+        self.tock = [[random.randint(0, 1) for _ in range(64)] for _ in range(32)]
+        if initPattern is not None:
+            for y in range(32):
+                for x in range(64):
+                    self.tick[y][x] = 0
+            for x, y in initPattern:
+                self.tick[y][x] = 1
+
+        self.palette = displayio.Palette(2)
+        self.palette[0] = colors[0]
+        self.palette[1] = colors[1]
+
+        self.bitmap = displayio.Bitmap(64, 32, 2)
+        self.tilegrid = displayio.TileGrid(
+            bitmap=self.bitmap, pixel_shader=self.palette
+        )
+        self.group = displayio.Group()
+        self.group.append(self.tilegrid)
+
+        self.directions = [
+            (63, 31),
+            (0, 31),
+            (1, 31),
+            (63, 0),
+            (1, 0),
+            (63, 1),
+            (0, 1),
+            (1, 1),
+        ]
+
+        self.ticktock = False
+
+    def update_anim(self):
+        if self.ticktock:
+            for y in range(32):
+                for x in range(64):
+                    popcount = 0
+                    for d in self.directions:
+                        popcount = (
+                            popcount + self.tick[(y + d[1]) % 32][(x + d[0]) % 64]
+                        )
+                    if popcount == 2:
+                        self.tock[y][x] = self.tick[y][x]
+                    elif popcount == 3:
+                        self.tock[y][x] = 1
+                    else:
+                        self.tock[y][x] = 0
+            for y in range(32):
+                for x in range(64):
+                    self.bitmap[x, y] = self.tock[y][x]
+            self.ticktock = False
+        else:
+            for y in range(32):
+                for x in range(64):
+                    popcount = 0
+                    for d in self.directions:
+                        popcount = (
+                            popcount + self.tock[(y + d[1]) % 32][(x + d[0]) % 64]
+                        )
+                    if popcount == 2:
+                        self.tick[y][x] = self.tock[y][x]
+                    elif popcount == 3:
+                        self.tick[y][x] = 1
+                    else:
+                        self.tick[y][x] = 0
+            for y in range(32):
+                for x in range(64):
+                    self.bitmap[x, y] = self.tick[y][x]
+            self.ticktock = True
+
+    def group(self):
+        return self.group
+
+    def name(self):
+        return self.name
+
+    def set_text(self, lineno, text):
+        pass
 
 
 class LinedGroup:
@@ -47,15 +201,18 @@ class LinedGroup:
         if fontchoice is not None:
             self.font = bitmap_font.load_font(f"fonts/{fontchoice}.bdf")
 
+        self.centered = False
+
         self.fontheight = self.font.get_bounding_box()[1]
         self.numlines = 32 // self.fontheight
+        self.yoffset = 16 - ((self.numlines * self.fontheight) // 2)
 
         if self.numlines == 0:
             self.numlines = 1
 
         self.lines = [
             adafruit_display_text.label.Label(
-                font=self.font, color=0x00A000, text=f"Line {i+1}"
+                font=self.font, color=textcolor, text=f"Line {i+1}"
             )
             for i in range(self.numlines)
         ]
@@ -63,7 +220,7 @@ class LinedGroup:
         self.group = displayio.Group()
         for i, line in enumerate(self.lines):
             line.x = 0
-            line.y = (i + 1) * self.fontheight - (self.fontheight // 2)
+            line.y = self.yoffset + ((i + 1) * self.fontheight - (self.fontheight // 2))
             self.group.append(line)
 
         self.name = name
@@ -78,9 +235,11 @@ class LinedGroup:
         return self.name
 
     def set_text(self, lineno, text):
-        if lineno > len(self.lines) or lineno < 0:
-            return  # should this be a silent error?
+        if lineno >= len(self.lines) or lineno < 0:
+            return  # TODO: should this be a silent error?
         self.lines[lineno].text = text
+        if self.centered:
+            self.lines[lineno].x = 32 - (self.lines[lineno].width // 2)
 
 
 class IHLRGB:
@@ -102,6 +261,14 @@ class IHLRGB:
 
         self.statetask = None
         self.displaytask = None
+
+        self.fliplatch = False
+
+        self.buttonup = None
+        self.buttondown = None
+
+        self.processup = False
+        self.processdown = False
 
     def run(self):
         while self.state != EXIT_STATE:
@@ -127,16 +294,50 @@ class IHLRGB:
             print(f"What state is this? {self.state}")
 
     def detect_inputs(self):
-        pass
+        if (
+            self.buttonup is not None
+            and self.buttondown is not None
+            and not self.processup
+            and not self.processdown
+        ):
+            if not self.buttonup.value:
+                time.sleep(0.05)
+                if not self.buttonup.value:
+                    self.processup = True
+                    print("Pressed up value")
+            if not self.buttondown.value:
+                time.sleep(0.05)
+                if not self.buttondown.value:
+                    self.processdown = True
+                    print("Pressed down value")
 
     def run_demo_tick(self):
-        self.displaynewstate = DISPLAY_DEMO1
         c = rtc.RTC().datetime
         hr = str(100 + c.tm_hour)[1:]
         mn = str(100 + c.tm_min)[1:]
         sc = str(100 + c.tm_sec)[1:]
-        self.demopage_line(0, f"{c.tm_mon}/{c.tm_mday}/{c.tm_year}")
-        self.demopage_line(1, f"{hr}:{mn}:{sc}")
+        yr = str(c.tm_year)[2:]
+        self.demopage_line(0, f"{hr}:{mn}:{sc}")
+        self.demopage_line(1, f"{c.tm_mon}/{c.tm_mday}/{yr}")
+        if not self.fliplatch:
+            if self.processup or self.processdown:
+                self.fliplatch = True
+                q = 0
+                if self.processup:
+                    q = 1
+                    self.processup = False
+                if self.processdown:
+                    q = -1
+                    self.processdown = False
+                idx = 0
+                for i, lbl in enumerate(DISPLAY_LIST):
+                    if self.displaystate == lbl:
+                        idx = i
+                        break
+                idx = (idx + q) % len(DISPLAY_LIST)
+                self.displaynewstate = DISPLAY_LIST[idx]
+        else:
+            self.fliplatch = False
 
         return RUN_DEMO_STATE
 
@@ -148,6 +349,7 @@ class IHLRGB:
             rtc.RTC().datetime = newtime.timetuple()
             print(f"New time: {time.localtime()}")
             time.sleep(1)
+        self.displaynewstate = DISPLAY_DEMO1
         return RUN_DEMO_STATE
 
     def refresh_screen(self):
@@ -163,7 +365,7 @@ class IHLRGB:
             ):
                 self.allgroups[self.displaystate].update_anim()
             while not self.display.refresh(minimum_frames_per_second=0):
-                time.sleep(0.001)
+                time.sleep(0.0001)
 
     def reset_display(self):
         displayio.release_displays()
@@ -195,7 +397,17 @@ class IHLRGB:
     def setup_screens(self):
         self.setup_bootscreen()
         self.setup_demoscreen()
+        self.setup_GOLscreen()
+        self.setup_CAGscreen()
         return SETUP_BOARD_STATE
+
+    def setup_GOLscreen(self):
+        golscreen = GameOfLifeAnimationGroup("gol")
+        self.allgroups[DISPLAY_GOL] = golscreen
+
+    def setup_CAGscreen(self):
+        cagscreen = ChaserAnimationGroup("cag")
+        self.allgroups[DISPLAY_CAG] = cagscreen
 
     def setup_bootscreen(self):
         bootscreen = LinedGroup(DISPLAY_BOOTING, "4x6")
@@ -214,7 +426,8 @@ class IHLRGB:
         self.refresh_screen()
 
     def setup_demoscreen(self):
-        demoscreen = LinedGroup(DISPLAY_DEMO1)
+        demoscreen = LinedGroup(DISPLAY_DEMO1, "t0-14-uni", 0x0080D0)
+        demoscreen.centered = True
         self.allgroups[DISPLAY_DEMO1] = demoscreen
 
         for i in range(len(demoscreen.lines)):
@@ -226,6 +439,13 @@ class IHLRGB:
 
     def setup_board(self):
         print(f"Booting up IHLRGB {IHLRGB_VERSION}")
+
+        self.buttondown = DigitalInOut(board.BUTTON_DOWN)
+        self.buttondown.direction = Direction.INPUT
+        self.buttondown.pull = Pull.UP
+        self.buttonup = DigitalInOut(board.BUTTON_UP)
+        self.buttonup.direction = Direction.INPUT
+        self.buttonup.pull = Pull.UP
 
         # all board pin settings
         spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
